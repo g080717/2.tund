a3742d80cdf26c09561dce7cfbd7fc900e91365c
Svetlana Natalova <svetnat@greeny.cs.tlu.ee>
2015-09-07 15:43:10 +0300

    login_error

--- user form.php
+++ user form.php
@@ -2,9 +2,35 @@
 	
 //user_form.php
 
+// jutum'rkide vahele input elemendi name
+//echo $_POST["email"];
+//echo $_POST["parool"];
 
+
+
+//kontrolli ainult siis kasutaja vajutab logi sisse nuppu
+if ($_SERVER["REQUEST_METHOD"] == "POST")   { 
+
+ 
+//kontrollime kasutaja eposti et see ei ole tühi"
+
+    if(empty($_POST["email"])){ 
+		$email_error = "Ei saa olla tühi"; 
 	
+	}
 
+//kontrollime parooli
+
+ if(empty($_POST["password"])){ 
+		$password_error = "Ei saa olla tühi"; 
+ }else{
+	 
+// parool ei tühi, kontrollime pikkust
+if (strlen($_POST["password"]) <8)  {
+	 $password_error = 
+	 }
+	 
+}
 ?>  
 <html>
 
@@ -14,11 +40,12 @@
 <body>
 
 <h2>login</h2>
-<input type="email" placeholder="e-post"> <br> <br>
-<input type="password" placeholder="parool">
+<form action="user form.php" method"get">
+<input name="email" type="email" placeholder="e-post"> <?php echo $email_error; ?><br> <br>
+<input name="parool" type="password" placeholder="parool">
 
 <input type="submit" value="Logi sisse">
-
+</form>
 <h2>create user</h2>
 
 
