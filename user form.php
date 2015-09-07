222adcd2272cddb02276cde2d18b106df33229ff
Svetlana Natalova <svetnat@greeny.cs.tlu.ee>
2015-09-07 14:59:36 +0300

    login

---
+++ user form.php
@@ -0,0 +1,26 @@
+<?php 
+	
+//user_form.php
+
+
+	
+
+?>  
+<html>
+
+<head>
+<title>user login page </title>
+</head>
+<body>
+
+<h2>login</h2>
+<input type="email" placeholder="e-post"> <br> <br>
+<input type="password" placeholder="parool">
+
+<input type="submit" value="Logi sisse">
+
+<h2>create user</h2>
+
+
+<head>
+</html>
--- werty.php
+++ werty.php
@@ -8,12 +8,39 @@ echo $first_name." " .$last_name;
 
 ?>
 
-<?php
-$age = 5;
+<br>
 
-// if (loogikatehe) 
-if () 
+<?php 
+	
+	$age = 5;
+	
+	// if( loogikatehe ){ tõene } else { väär }
+	if($age < 18) {
+		echo "alaealine";
+	} else {
+		echo "täisealine";
+	}
+?>  
 
-?>
+<br>
+
+<?php 
+	
+	for ($i = 0; $i < $age; $i = $i +1){ 
+
+echo "palju,".$i.",";
+
+	} 
+	echo "õnne";
+?>  
+
+<br>
+
+<?php 
+	
+echo date ("l, j F  Y e");
+?>  
+
+
 
 
